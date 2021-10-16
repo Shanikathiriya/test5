f960825ed99f0b96453270199b0b1c5d1be3152a
shani kathiriya <krishna.sanghani1210@gmail.com>
2021-10-16 20:42:32 +0100

    commit test3

---
+++ src/Githomework.java
@@ -0,0 +1,10 @@
+public class Githomework {
+    public static void main(String args [])
+    {
+        System.out.println("i like git");
+        System.out.println("i like git");
+        System.out.println("i like git");
+
+    }
+
+}
